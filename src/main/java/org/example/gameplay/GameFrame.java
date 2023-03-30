765dd00f4b9c8a57531fcbcd0f2127d73586cfd3
Krzysztof zpaedzki <krzysztofzapedzki@gmail.com>
2023-03-30 19:49:46 +0200

    keystrokesDone

--- src/main/java/org/example/gameplay/GameFrame.java
+++ src/main/java/org/example/gameplay/GameFrame.java
@@ -6,6 +6,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 import javax.swing.ActionMap;
 import javax.swing.InputMap;
+import javax.swing.KeyStroke;
+
 
 public class GameFrame extends JFrame {
 
@@ -13,9 +15,10 @@ public class GameFrame extends JFrame {
     public int screenWidth = (int) screenSize.getWidth();
     public int screenHeight = (int) screenSize.getHeight();
     private GameArea ga;
+    JFrame frame ;
 
     public void initComponents(){
-        JFrame frame = new JFrame("Tetris");
+        frame = new JFrame("Tetris");
 
         frame.setBounds(screenWidth/2-200,screenHeight/2-300,450, 637);
         frame.setDefaultCloseOperation(3);
@@ -30,9 +33,9 @@ public class GameFrame extends JFrame {
     }
 
     public GameFrame() {
-        initControls();
-        initComponents();
 
+        initComponents();
+        initControls();
         System.out.println("init controls");
 
         startGame();
@@ -43,15 +46,15 @@ public class GameFrame extends JFrame {
 
 
     private void initControls(){
-        InputMap im = this.getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
-        ActionMap am = this.getRootPane().getActionMap();
+        InputMap im = frame.getRootPane().getInputMap();
+        ActionMap am = frame.getRootPane().getActionMap();
 
-        im.put(KeyStroke.getKeyStroke(KeyEvent.VK_D,0),"right");
-        im.put(KeyStroke.getKeyStroke(KeyEvent.VK_A,0),"left");
-        im.put(KeyStroke.getKeyStroke(KeyEvent.VK_S,0),"down");
-        im.put(KeyStroke.getKeyStroke(KeyEvent.VK_W,0),"up");
+        im.put(KeyStroke.getKeyStroke("RIGHT"),"rightKEY");
+        im.put(KeyStroke.getKeyStroke("LEFT"),"leftKEY");
+        im.put(KeyStroke.getKeyStroke("DOWN"),"downKEY");
+        im.put(KeyStroke.getKeyStroke("UP"),"upKEY");
 
-        am.put("right", new AbstractAction() {
+        am.put("rightKEY", new AbstractAction() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 try{
@@ -63,19 +66,19 @@ public class GameFrame extends JFrame {
 
             }
         });
-        am.put("left", new AbstractAction() {
+        am.put("leftKEY", new AbstractAction() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 System.out.println("left");
             }
         });
-        am.put("down", new AbstractAction() {
+        am.put("downKEY", new AbstractAction() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 System.out.println("down");
             }
         });
-        am.put("up", new AbstractAction() {
+        am.put("upKEY", new AbstractAction() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 System.out.println("up");
